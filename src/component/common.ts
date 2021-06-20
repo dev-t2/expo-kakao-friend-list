d19245200ded303f06726a53cfef002cf19bcdea
t2 <oo_ri@naver.com>
2021-06-20 23:58:55 +0900

    Signup Component Style Update

--- src/component/common.ts
+++ src/component/common.ts
@@ -4,3 +4,8 @@ export const Box = styled.div(({ theme }) => ({
   backgroundColor: 'white',
   border: `1px solid ${theme.borderColor}`,
 }));
+
+export const BoldLink = styled.span({
+  fontWeight: 600,
+  color: '#8e8e8e',
+});
--- src/screen/Signup.tsx
+++ src/screen/Signup.tsx
@@ -1,26 +1,42 @@
 import { memo } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import styled from 'styled-components';
 
+import { BoldLink } from '../component/common';
 import { BottomLink, Button, Form, Input, Layout } from '../component/auth';
-import { SIGNUP } from '../route';
+import { HOME } from '../route';
+
+const HeaderContainer = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+const SubTitle = styled(BoldLink)({
+  textAlign: 'center',
+  marginTop: '10px',
+});
 
 const Signup = () => {
   return (
     <Layout>
       <Form>
-        <div>
+        <HeaderContainer>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
-        </div>
+          <SubTitle>친구들의 사진을 보려면 가입하세요.</SubTitle>
+        </HeaderContainer>
 
         <form>
-          <Input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
+          <Input type="email" placeholder="이메일 주소" />
+          <Input type="text" placeholder="이름" />
+          <Input type="text" placeholder="닉네임" />
           <Input type="password" placeholder="비밀번호" />
-          <Button type="submit">로그인</Button>
+          <Button type="submit">가입</Button>
         </form>
       </Form>
 
-      <BottomLink text="계정이 없으신가요?" link={SIGNUP} linkText="가입하기" />
+      <BottomLink text="계정이 있으신가요?" link={HOME} linkText="로그인" />
     </Layout>
   );
 };
