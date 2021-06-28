e9039184511944e94508596be2d80336048a466d
t2 <oo_ri@naver.com>
2021-06-28 16:06:28 +0900

    Card Action and Like Style Update

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -1,8 +1,15 @@
 import { memo } from 'react';
 import { gql, useQuery } from '@apollo/client';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import { getFeeds } from '../__generated__/getFeeds';
 import { Avatar, BoldText } from '../component/common';
+import {
+  faComment,
+  faHeart,
+  faPaperPlane,
+} from '@fortawesome/free-regular-svg-icons';
 
 const PhotoContainer = styled.div(({ theme }) => ({
   maxWidth: 615,
@@ -21,8 +28,27 @@ const Nickname = styled(BoldText)({
   marginLeft: 15,
 });
 
-const PhotoBody = styled.img({
+const PhotoContents = styled.img({
   minWidth: '100%',
+});
+
+const PhotoData = styled.div({
+  padding: 15,
+});
+
+const PhotoActions = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+});
+
+const PhotoAction = styled.div({
+  cursor: 'pointer',
+  marginRight: 15,
+});
+
+const Like = styled(BoldText)({
+  display: 'block',
+  marginTop: 15,
 });
 
 const GET_FEEDS_QUERY = gql`
@@ -55,7 +81,23 @@ const Home = () => {
             <Nickname>{feed?.user.nickname}</Nickname>
           </PhotoHeader>
 
-          <PhotoBody src={feed?.photoUrl} alt={`Feed ${feed?.id}`} />
+          <PhotoContents src={feed?.photoUrl} alt={`Feed ${feed?.id}`} />
+
+          <PhotoData>
+            <PhotoActions>
+              <PhotoAction>
+                <FontAwesomeIcon size="lg" icon={faHeart} />
+              </PhotoAction>
+              <PhotoAction>
+                <FontAwesomeIcon size="lg" icon={faComment} />
+              </PhotoAction>
+              <PhotoAction>
+                <FontAwesomeIcon size="lg" icon={faPaperPlane} />
+              </PhotoAction>
+            </PhotoActions>
+
+            <Like>좋아요 {feed?.like} 개</Like>
+          </PhotoData>
         </PhotoContainer>
       ))}
     </div>
