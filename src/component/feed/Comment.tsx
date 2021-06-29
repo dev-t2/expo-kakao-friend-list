5cf7c617c4313c8b61908e14b0d9b80dc6944d2d
t2 <oo_ri@naver.com>
2021-06-30 00:00:32 +0900

    Sanitize Setup and Comment Hash Tag Style Update

--- src/component/feed/Comment.tsx
+++ src/component/feed/Comment.tsx
@@ -1,13 +1,24 @@
 import { FC, memo } from 'react';
 import styled from 'styled-components';
+import sanitizeHtml from 'sanitize-html';
 
 import { BoldText } from '../common';
 
 const Container = styled.div({});
 
-const Caption = styled.span({
+const Caption = styled.span(({ theme }) => ({
   marginLeft: 10,
-});
+
+  mark: {
+    backgroundColor: 'inherit',
+    color: theme.primary,
+    cursor: 'pointer',
+
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+}));
 
 interface IComment {
   name?: string;
@@ -15,13 +26,19 @@ interface IComment {
 }
 
 const Comment: FC<IComment> = ({ name, contents }) => {
+  const sanitizedContents = sanitizeHtml(
+    contents?.replace(/#[\w]+/g, '<mark>$&</mark>') ?? '',
+    { allowedTags: ['mark'] }
+  );
+
   return (
     <Container>
       <BoldText>{name}</BoldText>
-      {contents && (
+
+      {sanitizedContents && (
         <Caption
           dangerouslySetInnerHTML={{
-            __html: contents.replace(/#[\w]+/g, '<mark>$&</mark>'),
+            __html: sanitizedContents,
           }}
         />
       )}
