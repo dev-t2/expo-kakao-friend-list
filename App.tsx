55a4e44d51015435237a6e4c19c824cd337c1111
dev-t2 <oo_ri@naver.com>
2023-02-04 15:48:02 +0900

    Emotion Install and Setup

--- App.tsx
+++ App.tsx
@@ -1,23 +1,24 @@
 import { memo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from '@emotion/react';
+import styled from '@emotion/native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import theme from './src/theme';
+
+const Container = styled.View({
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
 });
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <StatusBar style="auto" />
+      </Container>
+    </ThemeProvider>
   );
 };
 
